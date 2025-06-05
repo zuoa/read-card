38ca0220f6fa0ae0a04b509f9e4c1251c452e396
yujian <ijedyu@gmail.com>
2025-06-05 12:09:54 +0800

    1

--- utils.py
+++ utils.py
@@ -47,7 +47,7 @@ def get_markdown_from_url(url):
         'Accept': 'text/markdown',
     }
 
-    if '://mp.weixin.qq.com/' in url:
+    if '://mp.weixin.qq.com/' in url.lower():
         headers = {
             'Accept': 'text/markdown',
             'Authorization': f'Bearer {os.environ.get("JINA_READER_API_KEY", "")}',
